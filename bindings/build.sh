5750790e6c11bf21ff64cf8069f0f301bcd4a2b4
William Hua <william@attente.ca>
2019-02-04 11:37:19 -0500

    bindings: provide an api

--- bindings/build.sh
+++ bindings/build.sh
@@ -4,3 +4,5 @@ cargo build --release --target=wasm32-unknown-unknown
 mkdir lib 2>/dev/null || true
 cp target/wasm32-unknown-unknown/release/bindings.wasm lib
 wasm-bindgen --nodejs --out-dir lib lib/bindings.wasm
+yarn
+yarn tsc

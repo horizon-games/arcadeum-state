9024de662be102dd4b36ff4bfecb34197b14c386
William Hua <william@attente.ca>
2019-03-25 10:55:12 -0400

    bindings: rm -r bindings

--- bindings/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cargo build --release --target=wasm32-unknown-unknown
-mkdir lib 2>/dev/null || true
-cp target/wasm32-unknown-unknown/release/bindings.wasm lib
-wasm-bindgen --nodejs --out-dir lib lib/bindings.wasm
-yarn
-yarn tsc

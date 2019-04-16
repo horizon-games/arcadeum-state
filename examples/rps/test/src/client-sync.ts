57be9cdbeb441af0ba35aea39662f3d18d260fe5
William Hua <william@attente.ca>
2019-04-16 18:02:11 -0400

    rps/test: implementation

---
+++ examples/rps/test/src/client-sync.ts
@@ -0,0 +1,74 @@
+import * as polkadot from '@polkadot/api'
+import * as keyring from '@polkadot/keyring'
+import * as arcadeum from 'arcadeum-bindings'
+import * as rps from 'arcadeum-rps'
+import * as ethers from 'ethers'
+
+const account = ethers.Wallet.createRandom()
+process.send(account.address)
+
+const store = {
+  store: undefined as arcadeum.Store | undefined,
+  unsubscribe: undefined as (() => void) | undefined
+}
+
+process.on(`message`, async (message: any) => {
+  if (store.store === undefined) {
+    const rootMessage = ethers.utils.arrayify(message)
+    const send = (message: arcadeum.Message) =>
+      process.send(ethers.utils.hexlify(message.encoding))
+
+    store.store = new arcadeum.Store(
+      rps.Game,
+      rootMessage,
+      account,
+      new Uint8Array(),
+      (message: any) => {
+        console.log(`client (${process.pid}): ${JSON.stringify(message)}`)
+      },
+      send
+    )
+
+    await store.store.ready
+
+    while (store.store.nextPlayer !== undefined) {
+      await store.store.opponentActions
+
+      if (store.store.nextPlayer === store.store.player) {
+        let random = ethers.utils.randomBytes(1)[0]
+
+        while (random === 255) {
+          random = ethers.utils.randomBytes(1)[0]
+        }
+
+        await store.store.dispatch([random % 3])
+      }
+    }
+
+    switch (store.store.winner) {
+      case store.store.player:
+        console.log(`client (${process.pid}): ${account.address} won`)
+
+        const provider = new polkadot.WsProvider(`ws://localhost:9944`)
+        const api = await polkadot.ApiPromise.create(provider)
+        const keys = new keyring.Keyring({ type: `sr25519` })
+        const key = keys.addFromUri(`//Alice`)
+
+        api.tx.arcadeum
+          .prove(ethers.utils.hexlify(await store.store.proof))
+          .signAndSend(key)
+
+        break
+
+      case undefined:
+        console.log(`client (${process.pid}): ${account.address} drew`)
+        break
+
+      default:
+        console.log(`client (${process.pid}): ${account.address} lost`)
+        break
+    }
+  } else {
+    store.store.receive(ethers.utils.arrayify(message))
+  }
+})
---
+++ examples/rps/test/src/index.ts
@@ -0,0 +1,88 @@
+import * as arcadeum from 'arcadeum-bindings'
+import * as rps from 'arcadeum-rps'
+import * as child_process from 'child_process'
+import * as ethers from 'ethers'
+import * as path from 'path'
+
+const owner = ethers.Wallet.fromMnemonic(
+  `winter off snap small sleep debate cheap drill elevator glove caution once`
+)
+
+const client1 = child_process.fork(path.join(__dirname, `client-async`))
+const client2 = child_process.fork(path.join(__dirname, `client-sync`))
+
+const send = (player: arcadeum.Player, message: arcadeum.Message) => {
+  switch (player) {
+    case arcadeum.Player.One:
+      console.log(
+        `server (${process.pid}) > client 1 (${
+          client1.pid
+        }): ${ethers.utils.hexlify(message.encoding)}`
+      )
+
+      client1.send(ethers.utils.hexlify(message.encoding))
+      break
+
+    case arcadeum.Player.Two:
+      console.log(
+        `server (${process.pid}) > client 2 (${
+          client2.pid
+        }): ${ethers.utils.hexlify(message.encoding)}`
+      )
+
+      client2.send(ethers.utils.hexlify(message.encoding))
+      break
+  }
+}
+
+const server = {
+  account1: undefined as string | undefined,
+  account2: undefined as string | undefined,
+  server: undefined as arcadeum.Server | undefined
+}
+
+client1.on(`message`, (message: any) => {
+  console.log(`server (${process.pid}) < client 1 (${client1.pid}): ${message}`)
+
+  if (server.account1 === undefined) {
+    server.account1 = message
+
+    if (server.account2 !== undefined) {
+      server.server = new arcadeum.Server(
+        rps.Game,
+        owner,
+        server.account1,
+        server.account2,
+        new Uint8Array(),
+        new Uint8Array(),
+        new Uint8Array(),
+        send
+      )
+    }
+  } else {
+    server.server.receive(ethers.utils.arrayify(message))
+  }
+})
+
+client2.on(`message`, (message: any) => {
+  console.log(`server (${process.pid}) < client 2 (${client2.pid}): ${message}`)
+
+  if (server.account2 === undefined) {
+    server.account2 = message
+
+    if (server.account1 !== undefined) {
+      server.server = new arcadeum.Server(
+        rps.Game,
+        owner,
+        server.account1,
+        server.account2,
+        new Uint8Array(),
+        new Uint8Array(),
+        new Uint8Array(),
+        send
+      )
+    }
+  } else {
+    server.server.receive(ethers.utils.arrayify(message))
+  }
+})

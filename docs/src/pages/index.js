6db3a929eb067e5f8a6f4319f501a7b0e7c412a3
William Hua <william@attente.ca>
2020-01-16 12:36:38 -0500

    docs: index

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -12,8 +12,8 @@ const features = [
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Arcadeum was designed from the ground up to be easily installed and
+        used to get your Arcadeum game up and running quickly.
       </>
     ),
   },
@@ -22,18 +22,16 @@ const features = [
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Arcadeum lets you focus on your game, and we&apos;ll do the chores.
       </>
     ),
   },
   {
-    title: <>Powered by React</>,
+    title: <>Powered by Rust</>,
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Write your game logic in Rust. Compile down to WebAssembly.
       </>
     ),
   },
@@ -71,7 +69,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/guide')}>
               Get Started
             </Link>
           </div>

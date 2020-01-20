62be8b3bfd9ad87ebf189ec98633d45a36e11724
William Hua <william@attente.ca>
2020-01-20 11:30:11 -0500

    docs: links

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ module.exports = {
         src: 'img/logo.svg',
       },
       links: [
-        {to: 'docs/doc1', label: 'Docs', position: 'left'},
+        {to: 'docs/guide', label: 'User Guide', position: 'left'},
         {
           href: 'https://github.com/horizon-games/arcadeum-state',
           label: 'GitHub',

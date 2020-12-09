ade873611df1f19d403e70836a5986abb81ec936
William Hua <william@attente.ca>
2020-12-09 12:54:59 -0500

    std-tester: minimal test

--- std-tester/src/main.rs
+++ std-tester/src/main.rs
@@ -1,3 +1,54 @@
-fn main() {
-    println!("Hello, world!");
+/*
+ * Copyright 2020 Horizon Blockchain Games Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#![no_std]
+#![feature(start)]
+#![feature(lang_items)]
+#![feature(rustc_private)]
+#![feature(default_alloc_error_handler)]
+
+extern crate libc;
+
+#[allow(unused_imports)]
+use arcadeum;
+
+#[start]
+fn main(_argc: isize, _argv: *const *const u8) -> isize {
+    0
+}
+
+#[global_allocator]
+static ALLOCATOR: Allocator = Allocator;
+
+struct Allocator;
+
+unsafe impl core::alloc::GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, _: core::alloc::Layout) -> *mut u8 {
+        unimplemented!()
+    }
+
+    unsafe fn dealloc(&self, _: *mut u8, _: core::alloc::Layout) {
+        unimplemented!()
+    }
+}
+
+#[lang = "eh_personality"]
+fn eh_personality() {}
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
